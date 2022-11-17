cb47efd846bbc6f6e3550de0fba549e2299d4f1f
Thiago Vasconcelos <62908111+thiagogvasc@users.noreply.github.com>
2022-11-17 01:14:15 -0500

    Add dfs implementation to solve maze

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-console.log('hello world')
-
 const canvas = document.querySelector('canvas')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
@@ -28,8 +26,11 @@ class Cell {
 
         this.visited = false
 
-        this.fillColor = 'transparent'
+        //this.fillColor = 'transparent'
         this.strokeColor = 'black'
+
+        this.frameFillColor = 'transparent'
+        this.baseFillColor = 'transparent'
     }
 
     draw(ctx) {
@@ -55,15 +56,15 @@ class Cell {
             ctx.moveTo(this.x + this.width, this.y) 
             ctx.lineTo(this.x + this.width, this.y + this.height)
         }
-        ctx.closePath()
 
         ctx.strokeStyle = this.strokeColor
-        ctx.fillStyle = this.fillColor
+        ctx.fillStyle = this.frameFillColor
         ctx.stroke()
 
         ctx.beginPath()
         ctx.rect(this.x, this.y, this.width, this.height)
         ctx.fill()
+        this.frameFillColor = this.baseFillColor
     }
 
     update() {
@@ -80,6 +81,7 @@ class Grid {
         this.rows = rows
         this.cols = cols
         this.cells = []
+
         for (let i = 0; i < rows; i++) {
             const row = []
             for (let j = 0; j < cols; j++) {
@@ -94,14 +96,12 @@ class Grid {
 
     cellAt(i, j) {
         if (i < 0 || j < 0 || i >= this.cols || j >= this.rows) return undefined
-
         return this.cells[i][j]
     }
 
     draw(ctx) {
         ctx.beginPath()
         ctx.rect(this.x, this.y, this.width, this.height)
-        //ctx.strokeStyle = 'red'
         ctx.stroke()
         this.cells.forEach(row => row.forEach(cell => cell.draw(ctx)))
     }
@@ -111,73 +111,130 @@ class Grid {
     }
 }
 
+
+class MazeGenerator {
+    constructor() {
+
+    }
+
+    generateMaze(grid) {
+        const stack = []
+        stack.push(grid.cellAt(0, 0))
+
+        while (stack.length > 0) {
+        
+            const currCell = stack.pop()
+            const neighbors = []
+            
+            // top
+            if (grid.cellAt(currCell.i - 1, currCell.j)?.visited === false) {
+                neighbors.push(grid.cellAt(currCell.i - 1, currCell.j))
+            }
+    
+            // left
+            if (grid.cellAt(currCell.i, currCell.j - 1)?.visited === false) {
+                neighbors.push(grid.cellAt(currCell.i, currCell.j - 1))
+            }
+    
+            // bottom
+            if (grid.cellAt(currCell.i + 1, currCell.j)?.visited === false) {
+                neighbors.push(grid.cellAt(currCell.i + 1, currCell.j))
+            }
+    
+            // right
+            if (grid.cellAt(currCell.i, currCell.j + 1)?.visited === false) {
+                neighbors.push(grid.cellAt(currCell.i, currCell.j + 1))
+            }
+            
+            if (neighbors.length > 0) {
+                stack.push(currCell)
+            }
+    
+            const chosenCell = neighbors[Math.floor(Math.random()*neighbors.length)]
+    
+            if (chosenCell) {
+                //chosenCell.fillColor = 'blue'
+                chosenCell.visited = true
+                stack.push(chosenCell)
+                
+                if (chosenCell.i > currCell.i) {
+                    chosenCell.borders.top = false
+                    currCell.borders.bottom = false
+                } else if (chosenCell.i < currCell.i) {
+                    chosenCell.borders.bottom = false
+                    currCell.borders.top = false
+                } else if (chosenCell.j > currCell.j) {
+                    chosenCell.borders.left = false
+                    currCell.borders.right = false
+                } else { // else if (chosenCell.j < currCell.j) {
+                    chosenCell.borders.right = false
+                    currCell.borders.left = false
+                }
+            }
+        }
+    }
+}
+
 // State
-const grid = new Grid(10,10, 300, 300, 20, 20)
+const grid = new Grid(10, 10, 300, 300, 25, 25)
+const mazeGenerator = new MazeGenerator()
+mazeGenerator.generateMaze(grid)
+
 const stack = []
 stack.push(grid.cellAt(0, 0))
+const goalCell = grid.cellAt(10, 10)
+goalCell.baseFillColor = 'green'
 
+grid.cells.forEach(row => row.forEach(cell => cell.visited = false))
+let goalFound = false
 // Loop
 function mainLoop() {
-    
     grid.draw(ctx)
     grid.update()
 
-    if (stack.length > 0) {
+
+    if (stack.length > 0 && !goalFound) {
         
         const currCell = stack.pop()
-       // currCell.fillColor = 'red'
+        if (currCell.i === goalCell.i && currCell.j === goalCell.j) {
+            goalFound = true
+            console.log('end')
+            return
+        }
+        
+        currCell.frameFillColor = 'blue'
+        currCell.baseFillColor = 'red'
         const neighbors = []
         
         // top
-        if (grid.cellAt(currCell.i - 1, currCell.j)?.visited === false) {
+        if (grid.cellAt(currCell.i - 1, currCell.j)?.visited === false && currCell.borders.top === false) {
             neighbors.push(grid.cellAt(currCell.i - 1, currCell.j))
         }
 
         // left
-        if (grid.cellAt(currCell.i, currCell.j - 1)?.visited === false) {
+        if (grid.cellAt(currCell.i, currCell.j - 1)?.visited === false && currCell.borders.left === false) {
             neighbors.push(grid.cellAt(currCell.i, currCell.j - 1))
         }
 
         // bottom
-        if (grid.cellAt(currCell.i + 1, currCell.j)?.visited === false) {
+        if (grid.cellAt(currCell.i + 1, currCell.j)?.visited === false && currCell.borders.bottom === false) {
             neighbors.push(grid.cellAt(currCell.i + 1, currCell.j))
         }
 
         // right
-        if (grid.cellAt(currCell.i, currCell.j + 1)?.visited === false) {
+        if (grid.cellAt(currCell.i, currCell.j + 1)?.visited === false && currCell.borders.right === false) {
             neighbors.push(grid.cellAt(currCell.i, currCell.j + 1))
         }
-        console.log(neighbors)
+        
         if (neighbors.length > 0) {
             stack.push(currCell)
         }
 
-        const chosenCell = neighbors[Math.floor(Math.random()*neighbors.length)]
+        const chosenCell = neighbors[Math.floor(Math.random() * neighbors.length)]
+
         if (chosenCell) {
-            chosenCell.fillColor = 'blue'
             chosenCell.visited = true
             stack.push(chosenCell)
-            
-            console.log({chosenCell, currCell})
-            if (chosenCell.i > currCell.i) {
-                chosenCell.borders.top = false
-                currCell.borders.bottom = false
-            }
-
-            if (chosenCell.i < currCell.i) {
-                chosenCell.borders.bottom = false
-                currCell.borders.top = false
-            }
-
-            if (chosenCell.j > currCell.j) {
-                chosenCell.borders.left = false
-                currCell.borders.right = false
-            }
-
-            if (chosenCell.j < currCell.j) {
-                chosenCell.borders.right = false
-                currCell.borders.left = false
-            }
         }
     }
 }
